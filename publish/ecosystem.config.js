c73b07b8435161eac87c7b43f34e692a460aef98
bobby.li <bobby.li@ximalaya.com>
2018-08-12 13:47:22 +0800

    id path

--- publish/ecosystem.config.js
+++ publish/ecosystem.config.js
@@ -37,7 +37,7 @@ module.exports = {
       user: 'root',
       host: '47.75.185.211',
       ref: 'origin/master',
-      key: 'build/id_rsa',
+      key: 'publish/id_rsa',
       repo: 'git@github.com:Bobby90622/hunter.git',
       path: '/srv/hunter',
       'post-deploy': 'npm i && npm run publish_build && npm run publish_start',
